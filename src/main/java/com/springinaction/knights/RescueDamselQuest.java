a4d329a77fb89412bd979c14ec46352be1abfe95
Alex Meyer <alexalllowercase@gmail.com>
2022-01-16 20:45:56 +0000

    Show tight coupling between DamselRescuingKnight and RescueDamselQuest

---
+++ src/main/java/com/springinaction/knights/RescueDamselQuest.java
@@ -0,0 +1,8 @@
+package com.springinaction.knights;/**
+* Created by Alexander on 16/01/2022
+* COMMENTS ABOUT PROGRAM HERE */public class RescueDamselQuest{
+
+    public static void main (String [] args){
+     
+     }//main
+}//class

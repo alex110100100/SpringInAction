5b247d6a18801f6717496f9944cce3521f8d946f
Alex Meyer <alexalllowercase@gmail.com>
2022-01-18 22:25:15 +0000

    Demonstrate java-based config

--- src/main/java/com/springinaction/knights/KnightMain.java
+++ src/main/java/com/springinaction/knights/KnightMain.java
@@ -1,16 +1,15 @@
 package com.springinaction.knights;
 
-import org.springframework.context.support.
-      ClassPathXmlApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+//import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 
 public class KnightMain {
 
    public static void main(String[] args) throws Exception {
       //loads the beans from a config file and wires them together
-      ClassPathXmlApplicationContext context =
-            new ClassPathXmlApplicationContext(
-                  "META-INF/spring/knightbeans.xml");
+      AnnotationConfigApplicationContext context =
+            new AnnotationConfigApplicationContext(com.springinaction.knights.config.KnightBeans.class);
       Knight knight = context.getBean(Knight.class);
       knight.embarkOnQuest();
       context.close();

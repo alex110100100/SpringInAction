7258b74760de28eceda368e4568eadc7b7191594
Alex Meyer <alexalllowercase@gmail.com>
2022-01-19 20:11:57 +0000

    Demonstrate aspect-orientated programming

--- src/main/java/com/springinaction/knights/KnightMain.java
+++ src/main/java/com/springinaction/knights/KnightMain.java
@@ -1,15 +1,13 @@
 package com.springinaction.knights;
 
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-//import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 
 public class KnightMain {
 
    public static void main(String[] args) throws Exception {
       //loads the beans from a config file and wires them together
-      AnnotationConfigApplicationContext context =
-            new AnnotationConfigApplicationContext(com.springinaction.knights.config.KnightBeans.class);
+      ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("META-INF/spring/knightbeans.xml");
       Knight knight = context.getBean(Knight.class);
       knight.embarkOnQuest();
       context.close();
